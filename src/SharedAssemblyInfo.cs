9fde0ce73adff676920eca519814d89e7e8581ac
Mindaugas Mozuras <mindaugas.mozuras@gmail.com>
2010-12-28 21:22:02 +0200

    Added SharedAssemblyInfo

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ODataLambda")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ODataLambda")]
+[assembly: AssemblyProduct("ODataLambda")]
+[assembly: AssemblyCopyright("Copyright © Mindaugas Mozūras 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b7fdf82-303a-4425-a3b1-2141abb4b4ee")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
