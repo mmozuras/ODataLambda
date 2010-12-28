d35e161c0db3e9b2477ef29685f17a7612deb5ca
Mindaugas Mozuras <mindaugas.mozuras@gmail.com>
2010-12-28 23:09:16 +0200

    Added ability to expand nested properties in collection

--- src/ODataLambda.Tests/ODataExtensionsTests.cs
+++ src/ODataLambda.Tests/ODataExtensionsTests.cs
@@ -1,5 +1,6 @@
 namespace ODataLambda.Tests
 {
+    using System;
     using System.Data.Services.Client;
     using Fakes;
     using NUnit.Framework;
@@ -18,14 +19,40 @@ namespace ODataLambda.Tests
         [Test]
         public void Should_expand_single_property()
         {
-            var dataServiceQuery = fakeContext.Orders.Expand(x => x.Product);
+            var query = fakeContext.Orders.Expand(x => x.Product);
 
-            UriShouldContain(dataServiceQuery, "$expand=Product");
+            UriShouldContain(query, "$expand=Product");
         }
 
-        public static void UriShouldContain(DataServiceQuery query, string expected)
+        [Test]
+        public void Should_expand_nested_property()
         {
-            query.RequestUri.ToString().ShouldContain(expected);
+            var query = fakeContext.Orders.Expand(x => x.Product.Id);
+
+            UriShouldContain(query, "$expand=Product/Id");
+        }
+
+        [Test]
+        public void Should_expand_deeply_recursively_nested_property()
+        {
+            var query = fakeContext.Orders.Expand(x => x.Product.Order.Product.Order.Product.Order.Product.Order.Product.Order);
+
+            UriShouldContain(query, "$expand=Product/Order/Product/Order/Product/Order/Product/Order/Product/Order");
+        }
+
+        [Test]
+        public void Should_expand_in_collection_nested_property()
+        {
+            var query = fakeContext.Orders.Expand(x => x.Products.Expand(y => y.Order));
+
+            UriShouldContain(query, "$expand=Products/Order");
+        }
+
+        private static void UriShouldContain(DataServiceQuery query, string expected)
+        {
+            string requestUri = query.RequestUri.ToString();
+            Console.WriteLine(requestUri);
+            requestUri.ShouldContain(expected);
         }
     }
 }
--- src/ODataLambda/ODataExtensions.cs
+++ src/ODataLambda/ODataExtensions.cs
@@ -1,11 +1,9 @@
 namespace ODataLambda
 {
     using System;
-    using System.Collections.Generic;
     using System.Data.Services.Client;
     using System.Linq;
     using System.Linq.Expressions;
-    using System.Reflection;
 
     public static class ODataExtensions
     {
@@ -15,49 +13,38 @@ namespace ODataLambda
             return query.Expand(propertyName);
         }
 
-        private static string ToPropertyPath<T, TProperty>(this Expression<Func<T, TProperty>> expression)
+        public static string Expand<T, TProperty>(this DataServiceCollection<T> collection, Expression<Func<T, TProperty>> expression)
         {
-            IList<string> propertyNames = new List<string>();
-            Expression currentNode = expression.Body;
-            while (currentNode.NodeType != ExpressionType.Parameter)
-            {
-                if (currentNode.NodeType == ExpressionType.MemberAccess || currentNode.NodeType == ExpressionType.Convert)
-                {
-                    MemberExpression memberExpression = GetMemberExpression(currentNode);
-                    MemberInfo member = memberExpression.Member;
-
-                    if (!IsPropertyOrField(member))
-                    {
-                        throw new InvalidOperationException(string.Format("The member '{0}' is a {1} but a Property or Field is expected.", member.Name, member.MemberType));
-                    }
-                    propertyNames.Add(member.Name);
-                    currentNode = memberExpression.Expression;
-                }
-                else
-                {
-                    throw new InvalidOperationException(string.Format("The expression node type '{0}' is not supported. Expected: MemberAccess or Convert", currentNode.NodeType));
-                }
-            }
-            return string.Join("/", propertyNames.Reverse().ToArray());
+            return expression.ToPropertyPath();
         }
 
-        private static bool IsPropertyOrField(MemberInfo member)
+        private static string ToPropertyPath(this Expression expression)
         {
-            return member.MemberType == MemberTypes.Property || member.MemberType == MemberTypes.Field;
+            switch (expression.NodeType)
+            {
+                case ExpressionType.MemberAccess:
+                    var memberExpression = (MemberExpression) expression;
+                    var result = memberExpression.Expression.ToPropertyPath() + "/" + memberExpression.Member.Name;
+                    return RemoveLeadingSlash(result);
+
+                case ExpressionType.Lambda:
+                    return ((LambdaExpression)expression).Body.ToPropertyPath();
+
+                case ExpressionType.Call:
+                    var methodCallExpression = (MethodCallExpression) expression;
+                    var result2 = methodCallExpression.Arguments.Aggregate(string.Empty, (current, arg) => current + "/" + arg.ToPropertyPath());
+                    return RemoveLeadingSlash(result2);
+
+                case ExpressionType.Quote:
+                    var unaryExpression = (UnaryExpression) expression;
+                    return unaryExpression.Operand.ToPropertyPath();
+            }
+            return string.Empty;
         }
 
-        private static MemberExpression GetMemberExpression(Expression currentNode)
+        private static string RemoveLeadingSlash(string s)
         {
-            MemberExpression memberExpression;
-            if (currentNode.NodeType == ExpressionType.MemberAccess)
-            {
-                memberExpression = (MemberExpression)currentNode;
-            }
-            else
-            {
-                memberExpression = (MemberExpression)((UnaryExpression)currentNode).Operand;
-            }
-            return memberExpression;
+            return s.StartsWith("/") ? s.Remove(0, 1) : s;
         }
     }
 }
